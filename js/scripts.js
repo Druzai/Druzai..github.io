4d1a83ba7438500035f383a25fc9214ae5e94977
Druzai <j7world@yandex.ru>
2020-11-29 15:54:38 +0300

    Added 'about' page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 window.onscroll = function() {
+  let topButton = document.getElementById("top-button");
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
     document.getElementById("navbar").style.padding = "20px 10px";
     document.getElementById("logo").style.fontSize = "22px";
-    if (screen.width > 670) {
+    topButton.style.display = "block";
+    if (window.innerWidth > 670) {
       document.getElementById("navbar-top").style.padding = "45px 10px";
       document.getElementById("navbar-right").style.display = null;
     }
@@ -14,20 +16,22 @@ window.onscroll = function() {
   else {
     document.getElementById("navbar").style.padding = "50px 10px";
     document.getElementById("logo").style.fontSize = null;
-    if (screen.width > 670) {
+    topButton.style.display = null;
+    if (window.innerWidth > 670) {
         document.getElementById("navbar-top").style.padding = null;
         document.getElementById("navbar-right").style.display = null;
     }
     else {
       document.getElementById("navbar-top").style.padding = null;
-      document.getElementById("navbar-right").style.display = null;
+      setTimeout(() => {document.getElementById("navbar-right").style.display = null;}, 150);
+      //document.getElementById("navbar-right").style.display = null;
     }
   }
 };
 
 window.onresize = function() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    if (screen.width > 670) {
+    if (window.innerWidth > 670) {
       document.getElementById("navbar-right").style.display = null;
     }
     else {
@@ -35,7 +39,7 @@ window.onresize = function() {
     }
   } 
   else {
-    if (screen.width > 670) {
+    if (window.innerWidth > 670) {
         document.getElementById("navbar-right").style.display = null;
     }
     else {
@@ -44,10 +48,24 @@ window.onresize = function() {
   }
 };
 
+function goToTop() {
+  document.body.scrollTop = 0; // Sfari
+  document.documentElement.scrollTop = 0; // Chrome, FF, Opera
+}
+
+/*if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  } */
+
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function showDropDown() {
   document.getElementById("myDropdown").classList.toggle("show");
+  //document.getElementsByClassName("dropbtn")[0].innerHTML = "Статьи про ОС ▲"
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -56,14 +74,11 @@ window.onclick = function(e) {
     let myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
+      /*document.getElementsByClassName("dropbtn")[0].innerHTML = "Статьи про ОС ▼";*/
     }
   }
-}
-
-function getURL(URL){
-    let loc = window.location.pathname;
-    let dir = loc.substring(0, loc.lastIndexOf('/'));
-    if (dir.substring(dir.lastIndexOf('/') + 1) == "OS")
-        dir = dir.substring(0, dir.lastIndexOf('/'));
-    window.location.href = dir + '/' + URL;
+  /*
+  if (e.target.matches('.dropbtn') && myDropdown.classList.contains('show')) {
+    document.getElementsByClassName("dropbtn")[0].innerHTML = "Статьи про ОС ▼";
+  }*/
 }
